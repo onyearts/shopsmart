c6e6f2d691ec9610f780553c165d10a47187f4b0
onyearts <onyearts@gmail.com>
2025-08-04 10:01:43 +0100

    Update wsgi.py

--- marketing/wsgi.py
+++ marketing/wsgi.py
@@ -1,14 +1,15 @@
+"""
+WSGI config for marketing project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
 import os
-import sys
 
-# Add your project path to sys.path
-path = '/home/shopsmart/shopsmart'
-if path not in sys.path:
-    sys.path.append(path)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marketing.settings')
 
-# Set Django settings module
-os.environ['DJANGO_SETTINGS_MODULE'] = 'marketing.settings'
-
-# Load WSGI application
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
